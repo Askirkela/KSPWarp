666176ab3ef7420eb6ba602703c633a8edd54556
Askirkela <ldebene@gmail.com>
2018-03-19 19:02:21 +0100

    Trying to come back on it
    Huge commit with lots of corrections and upgrades

--- KSPWarp/KSPWarpClass.cs
+++ KSPWarp/KSPWarpClass.cs
@@ -1,103 +1,127 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace KSPWarp
 {
-	[KSPAddon(KSPAddon.Startup.MainMenu, true)]
+	[KSPAddon (KSPAddon.Startup.FlightAndKSC, true)]
 	public class KSPWarpClass : MonoBehaviour
 	{
-		private TimeWarp timewarp;
-		private static float[] baseRates,
-			superRates,
-			ultraRates;
-		private float[][] rates;
-
-		private void Start ()
-		{
-			print ("====== Loading KSPWarp ======");
-
-			timewarp = new TimeWarp ();
-			baseRates = new float[] {
-				1.0f,
-				5.0f,
-				10.0f,
-				50.0f,
-				100.0f,
-				1000.0f,
-				10000.0f,
-				100000.0f
-			};
+		private static String warpDebug = "[KSPWarp] : ";
+		private static float[] baseRates = new float[] {
+			1.0f,
+			5.0f,
+			10.0f,
+			50.0f,
+			100.0f,
+			1000.0f,
+			10000.0f,
+			100000.0f
+		},
 			superRates = new float[] {
-				1.0f,
-				10.0f,
-				50.0f,
-				100.0f,
-				1000.0f,
-				10000.0f,
-				100000.0f,
-				1000000.0f
-			};
+			1.0f,
+			10.0f,
+			50.0f,
+			100.0f,
+			1000.0f,
+			10000.0f,
+			100000.0f,
+			200000.0f
+		},
 			ultraRates = new float[] {
-				1.0f,
-				50.0f,
-				100.0f,
-				1000.0f,
-				10000.0f,
-				100000.0f,
-				1000000.0f,
-				10000000.0f
-			};
-			rates = new float[][] { baseRates, superRates, ultraRates };
-			setRates (0);
+			1.0f,
+			50.0f,
+			100.0f,
+			1000.0f,
+			10000.0f,
+			100000.0f,
+			200000.0f,
+			500000.0f
+		};
+		private static float[] baseAltitude = new float[] {
+			0.0f,
+			70000.0f,
+			70000.0f,
+			70000.0f,
+			120000.0f,
+			240000.0f,
+			480000.0f,
+			600000.0f
+		},
+			superAltitude = new float[] {
+			0.0f,
+			70000.0f,
+			70000.0f,
+			120000.0f,
+			240000.0f,
+			480000.0f,
+			600000.0f,
+			1000000.0f
+		},
+			ultraAltitude = new float[] {
+			0.0f,
+			70000.0f,
+			120000.0f,
+			240000.0f,
+			480000.0f,
+			600000.0f,
+			1000000.0f,
+			1000000.0f
+		};
+		private float[][] rates = new float[][] { baseRates, superRates, ultraRates };
+		private float[][] altitudes = new float[][] {
+			baseAltitude,
+			superAltitude,
+			ultraAltitude
+		};
+
+		void Start ()
+		{
+			print (warpDebug + "Launching KSPWarp");
 			UnityEngine.Object.DontDestroyOnLoad (this);
 		}
 
-		public void Update ()
+		void FixedUpdate ()
 		{
-			if (HighLogic.LoadedScene.Equals (GameScenes.TRACKSTATION) || HighLogic.LoadedScene.Equals (GameScenes.FLIGHT)) {
+			//				/** anti Kaboom thingy from Planet Factory **/
+			bool speedMax = (TimeWarp.fetch.warpRates.Equals (superRates) && (TimeWarp.fetch.current_rate_index == 7));
+			bool ultraMax = (TimeWarp.fetch.warpRates.Equals (ultraRates) && (TimeWarp.fetch.current_rate_index >= 6));
+			//
+			if (!speedMax || !ultraMax)
+				ToggleCollisions (true);
+			else
+				ToggleCollisions (false);
+		
+		}
 
-				RenderingManager.AddToPostDrawQueue (3, new Callback (drawGUI));
-				if (FindObjectOfType (typeof(TimeWarp)) != null)
-					this.timewarp = (TimeWarp)FindObjectOfType (typeof(TimeWarp));
-
-
-				/** anti Kaboom thingy from Planet Factory **/
-				bool speedMax = (this.timewarp.warpRates.Equals (superRates) && (this.timewarp.current_rate_index == 7));
-				bool ultraMax = (this.timewarp.warpRates.Equals (ultraRates) && (this.timewarp.current_rate_index >= 6));
-
-				if (!speedMax || ultraMax) 
-					ToggleCollisions (true);
-				else
-					ToggleCollisions (false);
-				
+		private void SetRates (int i)
+		{
+			if (i >= 0 || i < rates.Length) { 
+				TimeWarp.fetch.warpRates = rates [i];
+				TimeWarp.fetch.altitudeLimits = altitudes [i];
+				print (warpDebug + "Warp rates set to " + i);
+			} else {
+				SetRates (0);
+				print (warpDebug + "Warp rates set to default");
 			}
 		}
 
-		private void setRates (int i)
+		private void OnDestroy ()
 		{
-			if (i >= 0 || i <= 2) 
-				this.timewarp.warpRates = rates [i];
+			SetRates (0);	
 		}
-
-		int getCurrentRate ()
-		{
-			int currentRate;
-			if (this.timewarp.warpRates.Equals (baseRates))
-				currentRate = 0;
-			else if (this.timewarp.warpRates.Equals (superRates))
-				currentRate = 1;
-			else
-				currentRate = 2;
-			return currentRate;
-		}
-
 
 		/*******************************************
-        * 					GUI
-        * ******************************************/
+        	*			GUI
+        	* ******************************************/
 		protected Rect windowPos = new Rect (Screen.width / 6, 0, 75, 50);
 		bool basicBool, superBool, ultraBool;
+
+		private void OnGUI ()
+		{
+			DrawGUI ();
+		}
 
 		private void WindowGUI (int windowID)
 		{
@@ -113,55 +137,56 @@ namespace KSPWarp
 			ultraBool = GUILayout.Button ("Ultra", style, GUILayout.ExpandWidth (true));
 				
 			if (basicBool) { // If user clicks on Basic, set warp rates to stock
-				print ("==== Set rates 0 ====");
-				ScreenMessages.PostScreenMessage ("Entering Basic rates", 4, ScreenMessageStyle.UPPER_CENTER);
-				setRates (0);
+				ScreenMessages.PostScreenMessage (warpDebug + "Entering Basic rates", 4, ScreenMessageStyle.UPPER_CENTER);
+				SetRates (0);
 			}
 			if (superBool) { // If user clicks on Super, set warp rates to custom "super"
-				print ("==== Set rates 1 ====");
-				ScreenMessages.PostScreenMessage ("Entering Super rates", 4, ScreenMessageStyle.UPPER_CENTER);
-				setRates (1);
+				ScreenMessages.PostScreenMessage (warpDebug + "Entering Super rates", 4, ScreenMessageStyle.UPPER_CENTER);
+				SetRates (1);
 			}
 			if (ultraBool) { // If user clicks on Ultra, set warp rates to custom "ultra"
-				print ("==== Set rates 2 ====");
-				ScreenMessages.PostScreenMessage ("Entering Ultra rates", 4, ScreenMessageStyle.UPPER_CENTER);
-				setRates (2);
+				ScreenMessages.PostScreenMessage (warpDebug + "Entering Ultra rates", 4, ScreenMessageStyle.UPPER_CENTER);
+				SetRates (2);
 			}
 			GUILayout.EndHorizontal ();
 
 			GUI.DragWindow (new Rect (0, 0, 1000, 20));
 		}
 
-		private void drawGUI ()
+		private void DrawGUI ()
 		{
 			GUI.skin = HighLogic.Skin;
 			windowPos = GUILayout.Window (1, windowPos, WindowGUI, "Time warp rates", GUILayout.MinWidth (10));
-
 		}
 
 
 		/*******************************************
-		 * 			Collision handling
+		 * 		Collision handling
 		 * *****************************************/
 		bool isCollisionActive = true;
 
 		private void ToggleCollisions (bool enabled)
 		{
 			if (enabled != isCollisionActive) { // If collisions aren't already enabled/disabled
-
 				isCollisionActive = enabled;
 
 				//Enable/Disable collisions for all parts that makes the vessel
-				foreach (var vessel in FlightGlobals.Vessels) {
-					print ("**Debug : Collisions on " + vessel.GetName() + " are now " + enabled + "**");
-					foreach (var part in vessel.GetComponentsInChildren<Collider>())
-						part.enabled = enabled;
+				List<Vessel> vessels = FlightGlobals.Vessels;
+				Collider[] p;
+				for (int i = 0; i < vessels.Count; i++) {
+					print (warpDebug + "Collisions on " + vessels [i].GetName () + " are now " + enabled + "**");
+					ScreenMessages.PostScreenMessage (warpDebug + "Collisions on " + vessels [i].GetName () + " are now " + enabled + "**", 1, ScreenMessageStyle.UPPER_CENTER);
+					p = vessels [i].GetComponentsInChildren<Collider> ();
+					for (int j = 0; j < p.Length; j++)
+						p [j].enabled = enabled;
 				}
 
 				// Enable/Disable collisions for all Celestial bodies, just to be sure...
-				foreach (var body in FlightGlobals.Bodies) {
-					print ("**Debug : Collisions on " + body.GetName() + " are now " + enabled + "**");
-					body.GetComponentInChildren<Collider> ().enabled = enabled;
+				List<CelestialBody> bodies = FlightGlobals.Bodies;
+				for (int i = 0; i < bodies.Count; i++) {
+					print (warpDebug + "Collisions on " + bodies [i].GetName () + " are now " + enabled + "**");
+					ScreenMessages.PostScreenMessage (warpDebug + "Collisions on " + bodies [i].GetName () + " are now " + enabled + "**", 1, ScreenMessageStyle.UPPER_CENTER);
+					bodies [i].GetComponentInChildren<Collider> ().enabled = enabled;
 				}
 			}
 		}
